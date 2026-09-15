328f0cf1a4ea2307662e66c0c1e1383a0d4b975d
praticamentegiorni <56174247+praticamentegiorni@users.noreply.github.com>
2026-09-15 10:23:52 +0200

    Update AgendOne.py

--- AgendOne.py
+++ AgendOne.py
@@ -1794,12 +1794,12 @@ with tab4:
 with tab5:
     st.subheader("Registri di classe")
     try:
-        import RegistrOne
-        if hasattr(RegistrOne, "main"):
-            RegistrOne.main()
+        import streamlit_app
+        if hasattr(streamlit_app, "main"):
+            streamlit_app.main()
         else:
-            st.info("La procedura RegistrOne.py è stata importata con successo, ma non definisce una funzione main().")
+            st.info("La procedura streamlit_app.py è stata importata con successo, ma non definisce una funzione main().")
     except ImportError:
-        st.error("Impossibile trovare il file 'RegistrOne.py'. Assicurati che si trovi nella stessa cartella di 'AgendOne.py'.")
+        st.error("Impossibile trovare il file 'streamlit_app.py'. Assicurati che si trovi nella stessa cartella.")
     except Exception as e:
-        st.error(f"Errore durante l'esecuzione di RegistrOne.py: {e}")
+        st.error(f"Errore durante l'esecuzione di streamlit_app.py: {e}")
